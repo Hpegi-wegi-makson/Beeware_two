ead9141368030485b48ce0cb6ad48f1937c66178
Hpegi-wegi-makson <156901344+Hpegi-wegi-makson@users.noreply.github.com>
2024-12-19 11:41:29 +0300

    Add files via upload
    fg4tg43g34

--- app.py
+++ app.py
@@ -67,99 +67,97 @@ class HelloWorld(toga.App):
                 row_box.add(button)
             keyboard_container.add(row_box)
 
-        # Добавляем кнопку Backspace
-        backspace_button = toga.Button('⌫', on_press=self.backspace_pressed)
-        backspace_button.style.update(width=50, height=50)
-        keyboard_container.add(backspace_button)
+            # Добавляем кнопку Backspace
+            backspace_button = toga.Button('⌫', on_press=self.backspace_pressed)
+            backspace_button.style.update(width=50, height=50)
+            keyboard_container.add(backspace_button)
 
-        # Добавляем оба контейнера в основной бокс
-        main_box.add(top_container)
-        main_box.add(keyboard_container)
+            # Добавляем оба контейнера в основной бокс
+            main_box.add(top_container)
+            main_box.add(keyboard_container)
 
-        # Создаем главное окно приложения
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
+            # Создаем главное окно приложения
+            self.main_window = toga.MainWindow(title=self.formal_name)
+            self.main_window.content = main_box
+            self.main_window.show()
 
-        # Устанавливаем начальное активное поле
-        self.username_input.on_gain_focus = self.on_username_focus
-        self.password_input.on_gain_focus = self.on_password_focus
-        # Устанавливаем начальное активное поле
-        self.active_field = self.username_input
+            # Устанавливаем начальное активное поле
+            self.username_input.on_gain_focus = self.on_username_focus
+            self.password_input.on_gain_focus = self.on_password_focus
+            # Устанавливаем начальное активное поле
+            self.active_field = self.username_input
 
-    def on_username_focus(self, widget):
-        self.active_field = self.username_input
+        def on_username_focus(self, widget):
+            self.active_field = self.username_input
 
-    def on_password_focus(self, widget):
-        self.active_field = self.password_input
+        def on_password_focus(self, widget):
+            self.active_field = self.password_input
 
-    def button_pressed(self, widget):
-        # Добавляем текст кнопки в активное текстовое поле
-        self.active_field.value += widget.text
-
-
-    def create_table(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS logs (
-                timestamp TEXT,
-                login TEXT,
-                password TEXT,
-                result TEXT
-            );
-        ''')
-        self.conn.commit()
+        def button_pressed(self, widget):
+            # Добавляем текст кнопки в активное текстовое поле
+            self.active_field.value += widget.text
 
 
+        def create_table(self):
+            self.cursor.execute('''
+                CREATE TABLE IF NOT EXISTS logs (
+                    timestamp TEXT,
+                    login TEXT,
+                    password TEXT,
+                    result TEXT
+                );
+            ''')
+            self.conn.commit()
 
 
 
-    def backspace_pressed(self, widget):
-        # Удаляем последний символ из активного текстового поля
-        if self.active_field.value:
-            self.active_field.value = self.active_field.value[:-1]
+        def backspace_pressed(self, widget):
+            # Удаляем последний символ из активного текстового поля
+            if self.active_field.value:
+                self.active_field.value = self.active_field.value[:-1]
 
-        def submit_form(self, widget):
-            # Обработчик события нажатия на кнопку Submit
-            login = self.username_input.value
-            password = self.password_input.value
-            print(f"Login: {login}, Password: {password}")
+            def submit_form(self, widget):
+                # Обработчик события нажатия на кнопку Submit
+                login = self.username_input.value
+                password = self.password_input.value
+                print(f"Login: {login}, Password: {password}")
 
-            # Проверяем, введен ли корректный логин
-            if login:
-                if login == 'maks':
-                    greeting = 'GOOD job'
+                # Проверяем, введен ли корректный логин
+                if login:
+                    if login == 'maks':
+                        greeting = 'GOOD job'
+                    else:
+                        greeting = 'idiot'
+
+                    # Логируем запись в базу данных
+                    timestamp = str(datetime.now())
+                    self.cursor.execute(
+                        "INSERT INTO logs (timestamp, login, password, greeting) VALUES (?, ?, ?, ?)",
+                        (timestamp, login, password, greeting)
+                    )
+
+                    self.conn.commit()
+
+                    # Выводим сообщение
+                    dialog = toga.InfoDialog(title="Hi there!", message=greeting)
+                    dialog._show(self.main_window)
                 else:
-                    greeting = 'idiot'
-
-                # Логируем запись в базу данных
-                timestamp = str(datetime.now())
-                self.cursor.execute(
-                    "INSERT INTO logs (timestamp, login, password, greeting) VALUES (?, ?, ?, ?)",
-                    (timestamp, login, password, greeting)
-                )
-
-                self.conn.commit()
-
-                # Выводим сообщение
-                dialog = toga.InfoDialog(title="Hi there!", message=greeting)
-                dialog._show(self.main_window)
-            else:
-                # Ошибка, если логин пустой
-                dialog = toga.InfoDialog(title="Error", message="Please enter your login!")
-                dialog._show(self.main_window)
+                    # Ошибка, если логин пустой
+                    dialog = toga.InfoDialog(title="Error", message="Please enter your login!")
+                    dialog._show(self.main_window)
 
 
-    def on_usernamee_input_changed(self, widget):
-        self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        pass
-    def on_password_input_changed(self, widget):
-        self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        pass
+        def on_usernamee_input_changed(self, widget):
+            self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
+            pass
+        def on_password_input_changed(self, widget):
+            self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
+            pass
 
-    def activate_fields(self):
-        # Назначаем обработчики смены фокуса
-        self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
+        def activate_fields(self):
+            # Назначаем обработчики смены фокуса
+            self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
+            self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
 
 
 def main():
