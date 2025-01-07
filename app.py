4b1f705ea60569f4aec8c2b677e5cf7c81b5379d
Hpegi-wegi-makson <156901344+Hpegi-wegi-makson@users.noreply.github.com>
2025-01-07 19:11:20 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 import datetime
 import toga
@@ -6,95 +7,54 @@ from toga.style.pack import COLUMN, ROW
 
 
 class HelloWorld(toga.App):
-    def startup(self):
-        # Подключение к базе данных SQLite
-        self.conn = sqlite3.connect('C:/Users/Maksz/beeware-tutorial/database.db')
+    def startup(self, widget=None):
+        self.create_element_interface()
+        self.conn = sqlite3.connect('C://Users/Maksz//beeware-tutorial//database.db')  # таблицы
         self.cursor = self.conn.cursor()
-        
-        # Создание таблиц, если их ещё нет
-        self.create_table()
-
-        # Основной контейнер
-        main_box = toga.Box(style=Pack(direction=COLUMN))
-
-
-        # Контейнер для полей ввода и кнопки
-        top_container = toga.Box(style=Pack(direction=COLUMN, padding=25))
-
-        # Метка и текстовое поле для ввода логина
-        username_label = toga.Label("Username:", 
-        style=Pack
-        (padding=(5, 5)))
-        
-        self.username_input = toga.TextInput(readonly=True, style=Pack(flex=1,))
-
-        # Метка и текстовое поле для ввода пароля
-        password_label = toga.Label("Password:", style=Pack(padding=(5, 5)))
-        self.password_input = toga.PasswordInput(readonly=True, style=Pack(flex=1))
-
-
-        # Горизонтальные боксы для размещения меток и полей ввода
-        username_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        username_box.add(username_label)
-        username_box.add(self.username_input)
-
-        password_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        password_box.add(password_label)
-        password_box.add(self.password_input)
-
-        # Кнопка для отправки формы
-        submit_button = toga.Button(
-            "Submit", on_press=self.submit_form, style=Pack(padding=(10,10))
-        )
-
-        # Добавляем элементы в верхний контейнер
-        top_container.add(username_box)
-        top_container.add(password_box)
-        top_container.add(submit_button)
-
-        # Нижний контейнер для кнопок-клавиатуры
-        keyboard_container = toga.Box(
-            style=
-        Pack(direction=COLUMN,
-         padding=(1,750)))
-        
-
-        # Количество кнопок в одном ряду
-        buttons_per_row = 8  # уменьшаем количество кнопок в ряду, чтобы вместить Backspace
-
-        # Английский алфавит
-        alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-        # Создаем ряды кнопок
-        for row_index in range(len(alphabet) // buttons_per_row + 1):
-            row_box = toga.Box(style=Pack(direction=ROW))
-            for col_index in range(min(buttons_per_row, len(alphabet) - row_index * buttons_per_row)):
-                letter = alphabet[row_index * buttons_per_row + col_index]
-                button = toga.Button(letter, on_press=self.button_pressed)
-                button.style.update(width=50, height=50)
-                row_box.add(button)
-            keyboard_container.add(row_box)
-
-        # Добавляем кнопку Backspace
-        backspace_button = toga.Button('⌫', on_press=self.backspace_pressed)
-        backspace_button.style.update(width=50, height=50)
-        keyboard_container.add(backspace_button)
-
-        # Добавляем оба контейнера в основной бокс
-        main_box.add(top_container)
-        main_box.add(keyboard_container)
-
-        # Создаем главное окно приложения
+        self.sqsl()
+        self.main_box = toga.Box()  # окна
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
+        self.top_container = toga.Box(style=Pack(direction=COLUMN, padding=20, width=70, height=28))
 
-        # Устанавливаем начальное активное поле
+
+        self.keyboard_container = toga.Box(style=Pack(direction=COLUMN, padding=(150, 1)))
+        self.generate_keyboard()
+        self.main_window.content = self.main_box
+        self.main_window.show()
+        self.main_box.add(self.start_button)
         self.username_input.on_gain_focus = self.on_username_focus
         self.password_input.on_gain_focus = self.on_password_focus
         # Устанавливаем начальное активное поле
         self.active_field = self.username_input
-    
+
+    def show_keyboard(self):
+        self.main_box.add(self.keyboard_container)
+
+    def hide_keyboard(self):
+        if self.keyboard_container in self.main_box.children:
+            self.main_box.remove(self.keyboard_container)
+
+    def generate_keyboard(self):
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        buttons_per_row = 8
+        for row_index in range(len(alphabet) // buttons_per_row + 1):
+            row_box = toga.Box(style=Pack(direction=ROW))
+            for col_index in range(min(buttons_per_row, len(alphabet) - row_index * buttons_per_row)):
+                letter = alphabet[row_index * buttons_per_row + col_index]
+                button = toga.Button(
+                    letter,
+                    on_press=self.button_pressed,
+                    style=Pack(width=25, height=25,)
+                )
+                row_box.add(button)
+            self.keyboard_container.add(row_box)
+
+    def backspace_pressed(self, widget):
+        if self.active_field.value:
+            self.active_field.value = self.active_field.value[:-1]
+
+    def button_pressed(self, widget=None):
+        self.active_field.value += widget.text
 
     def on_username_focus(self, widget):
         self.active_field = self.username_input
@@ -102,79 +62,152 @@ class HelloWorld(toga.App):
     def on_password_focus(self, widget):
         self.active_field = self.password_input
 
-    def button_pressed(self, widget):
-        # Добавляем текст кнопки в активное текстовое поле
-        self.active_field.value += widget.text
 
 
-    def create_table(self):
+    def create_element_interface(self):
+        self.start_button = toga.Button("GO?: ", on_press=self.login_scrin)  # startup
+        self.scrin_button = toga.Button("Login in: ", on_press=self.main_screen)  # login_scrin
+        self.login_button = toga.Button("Login: ", on_press=self.login_screen)  # login_scrin
+        self.back_button = toga.Button("← login_screen", on_press=self.login_scrin)  # main_screen
+        self.back_two_button = toga.Button("← main_screen", on_press=self.main_screen)
+        self.login_print_button = toga.Button("Login inQ: ", on_press=self.сheck_to_register)  # main_screen
+        self.username_label = toga.Label("Username:", style=Pack(padding=(10, 10), flex=1, ))
+        self.username_input = toga.TextInput(readonly=True, style=Pack(flex=1, ))
+        self.password_label = toga.Label("Password:", style=Pack(padding=(10, 10), flex=1))
+        self.password_input = toga.PasswordInput(readonly=True, style=Pack(flex=1))
+        self.backspace_button = toga.Button('⌫', on_press=self.backspace_pressed)
+        self.backspace_button.style.update(width=25, height=25)
+        self.timestamp = str(datetime.datetime.now())
+        self.login = self.username_input.value
+        self.password = self.password_input.value
+        self.result = ''
+        self.username_input_login = toga.TextInput(readonly=True, style=Pack(flex=1, ))
+        self.username_label_login = toga.Label("Username:", style=Pack(padding=(10, 10), flex=1, ))
+        self.password_label_login = toga.Label("Password:", style=Pack(padding=(10, 10), flex=1))
+        self.password_input_login = toga.PasswordInput(readonly=True, style=Pack(flex=1))
+        self.log_button = toga.Button("Login: ", on_press=self.сheck_to_register)
+        
+
+        
+
+
+    def sqsl(self):
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS logs (
-                timestamp TEXT,
-                login TEXT,
-                password TEXT,
-                result TEXT
-            );
+        CREATE TABLE IF NOT EXISTS User (
+        timestamp TEXT,
+        login TEXT,
+        password TEXT,
+        result TEXT
+        )
         ''')
         self.conn.commit()
 
 
+    def login_scrin(self, widget=None):
+        self.main_box.remove(self.start_button)
+        self.main_box.add(self.login_button)
+        self.main_box.add(self.scrin_button)
+        if self.back_button in self.main_box.children:
+            self.main_box.remove(self.back_button)
 
-    def backspace_pressed(self, widget):
-        # Удаляем последний символ из активного текстового поля
-        if self.active_field.value:
-            self.active_field.value = self.active_field.value[:-1]
+        if self.login_print_button in self.top_container.children:
+            self.top_container.remove(self.login_print_button)
 
-    def submit_form(self, widget):
-        # Обработчик события нажатия на кнопку Submit
-        login = self.username_input.value
-        password = self.password_input.value
-        print(f"Login: {login}, Password: {password}")
+        if self.username_input in self.top_container.children:
+            self.top_container.remove(self.username_input)
 
-        # Проверяем, введен ли корректный логин
-        if login:
-            if login == 'maks':
-                greeting = 'GOOD job'
-            else:
-                greeting = 'idiot'
+        if self.username_label in self.top_container.children:
+            self.top_container.remove(self.username_label)
 
-            # Логируем запись в базу данных
-            timestamp = str(datetime.datetime.now())
-            self.cursor.execute(
-                "INSERT INTO logs (timestamp, login, password, result) VALUES (?, ?, ?, ?)",
-                (timestamp, login, password, greeting)
-            )
+        if self.password_input in self.top_container.children:
+            self.top_container.remove(self.password_input)
+        if self.password_label in self.top_container.children:
+            self.top_container.remove(self.password_label)
+        self.main_box.remove(self.username_input_login)
+        self.main_box.remove(self.username_label_login)
+        self.main_box.remove(self.password_input_login)
+        self.main_box.remove(self.password_label_login)
+        self.main_box.remove(self.log_button)
 
-            self.conn.commit()
+        self.hide_keyboard()
 
-            # Выводим сообщение
-            dialog = toga.InfoDialog(title="Hi there!", message=greeting)
-            dialog._show(self.main_window)
-        else:
+    def main_screen(self, widget=None):
+        self.top_container.add(self.username_input)
+        self.top_container.add(self.username_label)
+        self.top_container.add(self.password_input)
+        self.top_container.add(self.password_label)
+        self.top_container.add(self.login_print_button)
+        self.main_box.add(self.top_container)
+        self.keyboard_container.add(self.backspace_button)
+        self.show_keyboard()
+        self.main_box.add(self.back_button)
+        self.main_box.remove(self.login_button)
+        self.main_box.remove(self.scrin_button)
+
+    def login_screen(self, widget=None):
+        self.main_box.add(self.username_input_login)
+        self.main_box.add(self.username_label_login)
+        self.main_box.add(self.password_input_login)
+        self.main_box.add(self.password_label_login)
+        self.main_box.add(self.back_button)
+        self.main_box.add(self.log_button)
+        self.main_box.remove(self.scrin_button)
+        self.main_box.remove(self.login_button)
+        
+
+    def сheck_to_register(self, widget=None):
+        
+
+        current_login = "MAX"
+        current_password = "QWE"
+        
+        print(f"Login: {self.login}, Password: {self.password}")
+        login = self.username_input.value.lower()
+        if not login:
             # Ошибка, если логин пустой
             dialog = toga.InfoDialog(title="Error", message="Please enter your login!")
             dialog._show(self.main_window)
+            return 
+        
+        password = self.password_input.value.lower()
+        if not password:
+            # Ошибка, если логин пустой
+            dialog_2 = toga.InfoDialog(title="Error", message="Please enter your Passsword!")
+            dialog_2._show(self.main_window)
+            return 
+
+        current_login = current_login.lower()
+        current_password = current_password.lower()
+
+        if login == current_login and password == current_password:
+            self.result = "Nice"
+        elif login == current_login and password != current_password:
+            self.result = 'Invalid Password'
+        elif login != current_login and password == current_password:
+            self.result = 'Invalid Login'
+        else:
+            self.result = 'All Invalid Login'
+            
+        if self.result == "Nice":
+            dialog = toga.InfoDialog(title="Hi there!", message=self.result)
+        else:
+            dialog = toga.InfoDialog(title="Sueta", message=self.result)
+        dialog._show(self.main_window)
+
+        print("1",self.timestamp, self.username_input.value, self.password_input.value, self.result)
+        self.cursor.execute(
+            "INSERT INTO User (timestamp, login, password, result) VALUES (?, ?, ?, ?)",
+            (self.timestamp, self.username_input.value, self.password_input.value, self.result)
+        )
+
+        self.conn.commit()
+        
 
 
-    def on_usernamee_input_changed(self, widget):
-        self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        pass
-    def on_password_input_changed(self, widget):
-        self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        pass
 
-    def activate_fields(self):
-        # Назначаем обработчики смены фокуса
-        self.username_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
-        self.password_input.on_gain_focus = lambda w: self.focus_change_handler(w, True)
 
+
+        
 
 def main():
     return HelloWorld(formal_name='Hello World')
-
-
-# Запускаем приложение
-if __name__ == '__main__':
-    app = main()
-    app.activate_fields()  # Активируем отслеживание фокуса
-    app.main_loop()
